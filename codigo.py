ecd0480889c765cb07f8f5aae93de1cee61273dc
Soy Gabi <liviumusica@gmail.com>
2026-06-17 20:32:02 +0200

    Otra modificacion

--- codigo.py
+++ codigo.py
@@ -1,4 +1,6 @@
 nombre = "l"
 apellido = "d"
+edad = 10
+edad_futura = edad + 2
+print("Hola " + nombre + " " + apellido + " " +str(edad_futura) + " Como estas") 
 
-print("Hola " + nombre + " " + apellido + " Como estas") 
